45c4bbbf6e7d38b4945774864712f442fab385fe
benhmidawael <benhmidawael97@gmail.com>
2023-01-10 16:33:55 +0100

    last update

--- script.js
+++ script.js
@@ -1,82 +1,63 @@
 // Declaration 
-var hearts = document.getElementsByClassName("like");
-var qte = document.getElementsByClassName("Quant");
-var min = document.getElementsByClassName("minus-btn");
-var plus = document.getElementsByClassName("plus-btn");
-var del = document.getElementsByClassName("delete");
-var price = document.getElementsByClassName("price");
-var total = document.getElementById("finalPrice");
-var DivItems = document.querySelector(".items");
-var ItemDiv = document.querySelectorAll('.Item');
-updateTotal();
+let hearts = document.querySelectorAll(".like")
+let qte = document.querySelectorAll(".Quant")
+let min = document.querySelectorAll(".minus-btn")
+let plus = document.querySelectorAll(".plus-btn")
+let total = document.getElementById("finalPrice")
+let del = document.querySelectorAll(".delete")
+let DivItems = document.querySelector(".items")
+let ItemDiv = document.querySelectorAll('.Item')
 
+updateTotal()
+
+for (let i = 0; i < hearts.length; i++) {
+
+    // Change Heart Color
+    hearts[i].addEventListener('click', function () {
+        if (hearts[i].style.color == "red") {
+            hearts[i].style.color = "black"
+        } else {
+            hearts[i].style.color = "red"
+        }
+    })
+
+    // Plus Function & Update Total
+    plus[i].addEventListener('click', function () {
+        qte[i].value ++
+        updateTotal()
+    })
+
+// Min Function & Update Total
+    min[i].addEventListener('click', function () {
+        if (qte[i].value > 1) {
+            qte[i].value --
+            updateTotal()
+        }
+    })
+
+
+// Delete Function & Update Total
+    del[i].addEventListener('click', function () {
+        DivItems.removeChild(ItemDiv[i])
+        updateTotal()
+    })
 
 
 // change qte & Update Total 
-
-for (let i = 0; i < qte.length; i++) {
     qte[i].addEventListener('change', function () {
-        updateTotal();
-    });
+        updateTotal()
+    })
 }
 
+
+// function updte total price
 function updateTotal() {
-    var totalPrice = 0;
+    let price = document.querySelectorAll(".price")
+    let qte = document.querySelectorAll(".Quant")
+
+    let totalPrice = 0
     for (let i = 0; i < qte.length; i++) {
-        totalPrice += (parseInt(qte[i].value) * parseFloat(price[i].textContent));
+        totalPrice += qte[i].value * price[i].textContent
     }
-    total.value = totalPrice;
-}
-
-// Delete Function & Update Total
-
-for (let i = 0; i < del.length; i++) {
-    del[i].addEventListener('click', function () {
-        DivItems.removeChild(ItemDiv[i]);
-        updateTotal();
-    });
-}
-
-// Min Function & Update Total
-
-for (let i = 0; i < min.length; i++) {
-    min[i].addEventListener('click', function () {
-        minQte(qte[i]);
-    });
-}
-
-function minQte(qte) {
-    if (qte.value > 1) {
-        qte.value = parseInt(qte.value) - 1;
-        updateTotal();
-    }
-}
-
-// Plus Function & Update Total
-
-for (let i = 0; i < plus.length; i++) {
-    plus[i].addEventListener('click', function () {
-        plusQte(qte[i]);
-        updateTotal();
-    });
-}
-
-function plusQte(qte) {
-    qte.value = parseInt(qte.value) + 1;
-}
-
-// Change Heart Color
-
-for (let i = 0; i < hearts.length; i++) {
-    hearts[i].addEventListener('click', function () {
-        ChangeColor(this);
-    });
-}
-
-function ChangeColor(heart) {
-    if (heart.style.color == "red") {
-        heart.style.color = "black";
-    } else {
-        heart.style.color = "red";
-    }
+    total.value = totalPrice
 }
